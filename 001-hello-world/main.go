46489c4387794369a3a41c151184274301c3b976
Stanley Hayford <hayfordstanley@gmail.com>
2020-08-18 18:00:48 +0000

    Variables completed

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -5,5 +5,16 @@ import (
 )
 
 func main() {
-	fmt.Printf("Hello GoLang\n")
+	var age int64
+	age = 27
+
+	var isEmployed bool
+	isEmployed = true
+	fmt.Printf("Are you employed ? %t", isEmployed)
+
+	fmt.Print(age)
+	var message = "Hello, Playground"
+	var greeting, name = "Welcome", "Stanley"
+	fmt.Println(message)
+	fmt.Println(greeting, name)
 }
